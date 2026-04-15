e8219327b0c9d813b9189b00a91d20913c10267c
muhammedguncum <muhammedguncum@outlook.com>
2026-04-15 18:08:25 +0300

    Proje dosyası ekle.

---
+++ LogAnalyzer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = "server_logs.txt";
+        Console.WriteLine("Program başladı...\n");
+
+        if (!File.Exists(filePath))
+        {
+            Console.WriteLine("Log file not found!");
+            return;
+        }
+
+        string[] lines = File.ReadAllLines(filePath);
+        Console.WriteLine($"{lines.Length} satır başarıyla okundu.");
+    }
+}
